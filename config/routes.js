6d583099a72075ab099d2181b7b098b4fc8ee284
Chad Jemmett <chad.jemmett@gmail.com>
2018-11-30 09:52:31 -0700

    can generate token

--- config/routes.js
+++ config/routes.js
@@ -1,4 +1,10 @@
+const knex = require('knex')
+const knexConfig = require('../knexfile.js')
+const db = knex(knexConfig.development)
 const axios = require('axios');
+const bcrypt = require('bcryptjs')
+const jwtKey = require('../_secrets/keys').jwtKey;
+const jwt = require('jsonwebtoken');
 
 const { authenticate } = require('./middlewares');
 
@@ -8,11 +14,46 @@ module.exports = server => {
   server.get('/api/jokes', authenticate, getJokes);
 };
 
+generateToken = (user) => {
+  const payload = {
+    subject: user.id,
+    username: user.username,
+  }
+
+  const secret = jwtKey
+
+
+    const options = {
+      expiresIn: '1h',
+    }
+
+  return jwt.sign(payload, secret, options)
+}
+
 function register(req, res) {
+  const creds = req.body
+  const hash = bcrypt.hashSync(creds.password, 2)
+  creds.password = hash;
+  db('users').insert(creds).then(ids => {
+    res.status(201).json(ids)
+  }).catch(err => res.status(500).json(err))
   // implement user registration
 }
 
 function login(req, res) {
+  const creds = req.body
+  db('users').where({username: creds.username}).first()
+  .then(user => {
+    if(user && bcrypt.compareSync(creds.password, user.password)) {
+      const token = generateToken(user)
+      res.status(200).json({message: 'welcome user', token})
+
+      //generate token
+    } else {
+      res.status(401).json({message: 'You shall not pass'})
+    }
+
+  })
   // implement user login
 }
 
